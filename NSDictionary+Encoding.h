cc24f10cf1179d9453bc25aea039af82d6e797ab
CL <1260807001@qq.com>
2018-05-17 17:50:58 +0800

    add more files

---
+++ NSDictionary+Encoding.h
@@ -0,0 +1,18 @@
+//
+//  NSDictionary+Encoding.h
+//  HSZTF
+//
+//  Created by CL on 2017/2/28.
+//  Copyright © 2017年 CL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Encoding)
+
+
+-(NSString *)descrip ;
+- (NSDictionary *)deleteNull ;
+
+
+@end
